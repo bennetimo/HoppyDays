be9ec8d4c67716bb5a045c57eac5ee0aa27fb101
Tim Bennett <2475478+bennetimo@users.noreply.github.com>
2019-08-13 00:19:28 +0100

    bunny can move left/right and jump

---
+++ Player/Player.gd
@@ -0,0 +1,30 @@
+extends KinematicBody2D
+
+var motion = Vector2(0,0)
+
+const SPEED = 300
+const GRAVITY = 300
+const UP = Vector2(0, -1)
+const JUMP_SPEED = 2000
+
+func _physics_process(delta):
+	apply_gravity()
+	jump()
+	move()
+	motion = move_and_slide(motion, UP)
+	
+func apply_gravity():
+#	if not is_on_floor():
+		motion.y += GRAVITY
+#	else: motion.y = 0
+	
+func jump():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		motion.y -= JUMP_SPEED
+		
+func move():
+	if Input.is_action_pressed("left") and !Input.is_action_pressed("right"):
+		motion.x = -SPEED
+	elif Input.is_action_pressed("right") and !Input.is_action_pressed("left"):
+		motion.x = SPEED
+	else: motion.x = 0

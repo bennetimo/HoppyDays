d144dceeb7720bfe4d01b3f493fcc435c75c54d7
Tim Bennett <2475478+bennetimo@users.noreply.github.com>
2019-08-13 19:48:03 +0100

    animate bunny

--- Player/Player.gd
+++ Player/Player.gd
@@ -5,12 +5,13 @@ var motion = Vector2(0,0)
 const SPEED = 300
 const GRAVITY = 300
 const UP = Vector2(0, -1)
-const JUMP_SPEED = 2000
+const JUMP_SPEED = 2500
 
 func _physics_process(delta):
 	apply_gravity()
 	jump()
 	move()
+	animate()
 	motion = move_and_slide(motion, UP)
 	
 func apply_gravity():
@@ -19,7 +20,7 @@ func apply_gravity():
 #	else: motion.y = 0
 	
 func jump():
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_pressed("jump") and is_on_floor():
 		motion.y -= JUMP_SPEED
 		
 func move():
@@ -28,3 +29,11 @@ func move():
 	elif Input.is_action_pressed("right") and !Input.is_action_pressed("left"):
 		motion.x = SPEED
 	else: motion.x = 0
+
+func animate():
+	if motion.y < 0:
+		$AnimatedSprite.play("jump")
+	elif motion.x != 0:
+		$AnimatedSprite.flip_h = (motion.x < 0)
+		$AnimatedSprite.play("walk")
+	else: $AnimatedSprite.play("idle")

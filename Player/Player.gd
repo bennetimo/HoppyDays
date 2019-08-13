124fa76fa33c2b5c8f56e84bd83b02b12cd4d117
Tim Bennett <2475478+bennetimo@users.noreply.github.com>
2019-08-13 22:11:28 +0100

    add audio

--- Player/Player.gd
+++ Player/Player.gd
@@ -32,6 +32,7 @@ func apply_gravity():
 func jump():
 	if Input.is_action_pressed("jump") and is_on_floor():
 		motion.y -= JUMP_SPEED
+		$JumpSFX.play()
 		
 func move():
 	if Input.is_action_pressed("left") and !Input.is_action_pressed("right"):
@@ -49,5 +50,6 @@ func end_game():
 func hurt():
 	motion.y -= JUMP_SPEED	
 	lives -= 1
+	$PainSFX.play()
 	if lives < 0:
 		end_game()

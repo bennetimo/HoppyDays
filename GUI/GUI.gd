d6cb3d46493599e1e6b47348922ddecf83ef0f64
Tim Bennett <2475478+bennetimo@users.noreply.github.com>
2019-09-03 00:43:06 +0100

    add coins

--- GUI/GUI.gd
+++ GUI/GUI.gd
@@ -3,5 +3,9 @@ extends CanvasLayer
 func set_life_count(lives):
 	$Control/TextureRect/HBoxContainer/LifeCount.text = str(lives)
 	
-func update_lives(lives: int):
+func set_coin_count(coins):
+	$Control/TextureRect/HBoxContainer/CoinCount.text = str(coins)	
+	
+func update_gui(lives: int, coins: int):
 	set_life_count(lives)
+	set_coin_count(coins)	
--- Levels/GameState.gd
+++ Levels/GameState.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 var lives = 3
+var coins = 0
+var target_coins_for_life = 3
 
 func _ready():
 	add_to_group("Gamestate")
@@ -13,8 +15,19 @@ func hurt():
 	if lives <= 0:
 		end_game()
 		
+func coin_up():
+	coins += 1
+	update_GUI()
+	var enough_coins = (coins % target_coins_for_life) == 0		
+	if enough_coins:
+		life_up()
+		
+func life_up():
+	lives += 1
+	update_GUI()
+		
 func update_GUI():
-	get_tree().call_group("GUI", "update_lives", lives) 		
+	get_tree().call_group("GUI", "update_gui", lives, coins)
 		
 func end_game():
 	get_tree().change_scene("res://Levels/GameOver.tscn")
